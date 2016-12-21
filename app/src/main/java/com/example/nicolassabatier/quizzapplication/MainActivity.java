1983bb83cee12a26dc1a3ebb06e3d71915399894
dylangreed <dylanlim@hotmail.fr>
2016-12-21 18:15:16 +0000

    Class for each country Method that create list of random flag Layout for list of flag Change drawable resource to lower case without space New Button on mainactivity that changes layout and activity

--- app/src/main/java/com/example/nicolassabatier/quizzapplication/MainActivity.java
+++ app/src/main/java/com/example/nicolassabatier/quizzapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nicolassabatier.quizzapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,21 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mOnePlayer;
     private Button mTwoPlayer;
 
-//    private ImageButton mCountry1;
-//    private ImageButton mCountry2;
-//    private ImageButton mCountry3;
 
-//    private Country[] mCountryBank = new Country[] {
-//            new Country(R.drawable.asean, "ASEAN"),
-//            new Country(R.drawable.afghanistan, "Afghanistan"),
-//            new Country(R.drawable.albania, "Albania"),
-//            new Country(R.drawable.bahamas, "Bahamas"),
-//            new Country(R.drawable.bangladesh, "Bangladesh"),
-//            new Country(R.drawable.cambodja, "Cambodja"),
-//            new Country(R.drawable.vanutau, "Vanutau"),
-//    };
-
-//    ListOfQuestion mListOfQuestion = new ListOfQuestion(mCountryBank);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +26,12 @@ public class MainActivity extends AppCompatActivity {
         });
         mTwoPlayer = (Button) findViewById(R.id.twoPlayer);
 
-//        setContentView(R.layout.listofquestion);
-//        mCountry1 = (ImageButton) findViewById(R.id.question_1);
-//        mCountry2 = (ImageButton) findViewById(R.id.question_2);
-//        mCountry1.setImageResource(mListOfQuestion.getQuestionFlag(0));
-//        mCountry2.setImageResource(mListOfQuestion.getQuestionFlag(1));
-//        mCountry3 = (ImageButton) findViewById(R.id.test);
-//        mCountry3.setImageResource(R.drawable.afghanistan);
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, QuestionListActivity.class);
+        startActivity(intent);
     }
 
 }
