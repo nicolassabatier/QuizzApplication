d3c79d1dd5b0f48d8365e8873a426d2099067e5a
dylangreed <dylanlim@hotmail.fr>
2016-12-26 19:01:41 +0000

    Merge branch 'CountryClass' of /Users/dylangreed/Documents/QuizzApplication with conflicts.

--- app/src/main/java/com/example/nicolassabatier/quizzapplication/Country.java
+++ app/src/main/java/com/example/nicolassabatier/quizzapplication/Country.java
@@ -1,11 +1,15 @@
 package com.example.nicolassabatier.quizzapplication;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by dylangreed on 14/12/2016.
  * Object class for a country that save the flag and the name
  */
 
-public class Country {
+
+public class Country implements Parcelable {
 
     private int mFlagResId; // gives the id resource for icon in drawable res
     private String mName;  // Associated name
@@ -32,4 +36,33 @@ public class Country {
         mName = name;
     }
 
+
+    protected Country(Parcel in) {
+        mFlagResId = in.readInt();
+        mName = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mFlagResId);
+        dest.writeString(mName);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Country> CREATOR = new Parcelable.Creator<Country>() {
+        @Override
+        public Country createFromParcel(Parcel in) {
+            return new Country(in);
+        }
+
+        @Override
+        public Country[] newArray(int size) {
+            return new Country[size];
+        }
+    };
 }
