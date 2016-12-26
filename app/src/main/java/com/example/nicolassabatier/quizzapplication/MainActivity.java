10c6495b8facbae73c642a2500de0c016d300993
Nicolassabatier <zcahnsa@ucl.ac.uk>
2016-12-26 18:45:41 +0100

    J'ai commencé à faire une database pour les noms, Rajout d'un layout registration quand tu appuies sur le bouton 1 et 2 +class Players

--- app/src/main/java/com/example/nicolassabatier/quizzapplication/MainActivity.java
+++ app/src/main/java/com/example/nicolassabatier/quizzapplication/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.nicolassabatier.quizzapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+import static com.example.nicolassabatier.quizzapplication.R.string.name;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button mOnePlayer;
     private Button mTwoPlayer;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mOnePlayer = (Button) findViewById(R.id.onePlayer);
+        mOnePlayer = (Button) findViewById(R.id.One_playerButton);
         mOnePlayer.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                System.out.print("Lol");
+                public void onClick(View v) {
+                final Player aplayer = new Player();
+
+                        Intent intention = new Intent(MainActivity.this,RegistrationActivity.class);
+                        startActivity(intention);
             }
         });
-        mTwoPlayer = (Button) findViewById(R.id.twoPlayer);
+        mTwoPlayer = (Button) findViewById(R.id.Two_playersButton);
+
     }
+
+
 }
