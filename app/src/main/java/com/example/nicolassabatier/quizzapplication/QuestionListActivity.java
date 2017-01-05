cc947468f0177a5863304eadc76c81da69652bc2
dylangreed <dylanlim@hotmail.fr>
2017-01-05 12:38:56 +0100

    Class for each country Method that create list of random flag Layout for list of flag Change drawable resource to lower case without space New Button on mainactivity that changes layout and activity

--- app/src/main/java/com/example/nicolassabatier/quizzapplication/QuestionListActivity.java
+++ app/src/main/java/com/example/nicolassabatier/quizzapplication/QuestionListActivity.java
@@ -1,30 +1,33 @@
 package com.example.nicolassabatier.quizzapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
 public class QuestionListActivity extends AppCompatActivity {
 
+    private ListOfQuestion mListOfQuestion;
+    private static final String LIST_OF_QUESTION =
+            "listquestion";
+
     private ImageButton mCountry1;
     private ImageButton mCountry2;
     private ImageButton mCountry3;
 
-    public Country[] mCountryBank = new Country[] {
-            new Country(R.drawable.asean, "ASEAN"),
-            new Country(R.drawable.afghanistan, "Afghanistan"),
-            new Country(R.drawable.albania, "Albania"),
-            new Country(R.drawable.bahamas, "Bahamas"),
-            new Country(R.drawable.bangladesh, "Bangladesh"),
-            new Country(R.drawable.cambodja, "Cambodja"),
-            new Country(R.drawable.vanutau, "Vanutau"),
-    };
-
-    ListOfQuestion mListOfQuestion = new ListOfQuestion(mCountryBank);
+    public static Intent newIntent(Context packageContext, ListOfQuestion mListQuestion) {
+        Intent i = new Intent(packageContext, QuestionListActivity.class);
+        i.putExtra(LIST_OF_QUESTION, mListQuestion);
+        return i;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle bundle = getIntent().getExtras();
+        mListOfQuestion = bundle.getParcelable(LIST_OF_QUESTION);
 
         setContentView(R.layout.listofquestion);
         mCountry1 = (ImageButton) findViewById(R.id.question_1);
